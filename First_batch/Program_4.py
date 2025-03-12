4a102ece7d688a7587914613a65241856415cb0f
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 11:26:23 +0800

    editing_variables_prog4

--- First_batch/Program_4.py
+++ First_batch/Program_4.py
@@ -1,6 +1,6 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
-first_value = int(input("Input an integer: "))
-second_value = int(input("Input a second integer: "))
-product = first_value * second_value 
+value_1 = int(input("Input an integer: "))
+value_2 = int(input("Input a second integer: "))
+product = value_1 * value_2 
 print (f"the product is {product}")
