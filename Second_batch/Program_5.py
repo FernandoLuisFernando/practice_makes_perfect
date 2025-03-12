c476340329350116207748e500a94bb568e83052
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 13:00:10 +0800

    making_program_5_batch_2

---
+++ Second_batch/Program_5.py
@@ -0,0 +1,7 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+value_1 = int(input("enter a number: "))
+value_2 = int(input("enter a number: "))
+remainder_result = value_1 % value_2
+print (f"The remainder is {remainder_result}.")
+
