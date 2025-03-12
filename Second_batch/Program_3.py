134d57da162368284528f891707fbe8070487e83
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 12:32:21 +0800

    making_program_3_batch_2

---
+++ Second_batch/Program_3.py
@@ -0,0 +1,6 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+value_1 = int(input("Enter an integer: "))
+value_2 = int(input("Enter an integer: "))
+difference = value_1 - value_2
+print (f"the result is {difference}. ")
