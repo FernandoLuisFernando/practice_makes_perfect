1b75865a3b61eb263cbcd04b0eb1f546a68faa05
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 13:25:57 +0800

    program_10_batch_3

---
+++ Second_batch/2 Program_last.py
@@ -0,0 +1,8 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+value_1 = int(input("enter the first number: "))
+value_2 = int(input("enter the second number: "))
+if value_1 >  value_2:
+    value_1, value_2 == value_2, value_1
+print("Numbers between the two numbers: ")
+for num in range (value_1 + 1, value_2):
+    print(num)
