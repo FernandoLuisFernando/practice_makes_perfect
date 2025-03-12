1bee1fc035af576880ceed87b0c0e5514eb0175b
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 11:22:00 +0800

    making_program_3

---
+++ First_batch/Program_3.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+value_1 = int(input("Enter your first value: "))
+value_2 = int(input("Enter your first value: "))
+sum = value_1 + value_2 
+print (f"the sum is {sum}")
