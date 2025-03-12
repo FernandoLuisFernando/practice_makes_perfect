28baff146e7231f77614d975064d9915a75120ed
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 13:57:21 +0800

    edited_program_1_batch_3

--- Third_batch/3_Program_1.py
+++ Third_batch/3_Program_1.py
@@ -1,5 +1,10 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+numbers = []
 for loop in range(10):
     num = int(input(f"Enter a number {loop+1}: "))
-if num != num:
-    print(num) 
+    numbers.append(num)
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num)
+
