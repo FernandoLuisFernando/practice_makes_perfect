a702d865be32ad61783aa920ba8f140bbf9c8550
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 11:24:43 +0800

    making_program_4

---
+++ First_batch/Program_4.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+first_value = int(input("Input an integer: "))
+second_value = int(input("Input a second integer: "))
+product = first_value * second_value 
+print (f"the product is {product}")
