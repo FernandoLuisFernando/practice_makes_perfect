5eb4f6e2c6031bde611679c5fa793cf8fbe49d9d
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-03-12 21:13:56 +0800

    3_program_2_batch_3

---
+++ Third_batch/3_program_2.py
@@ -0,0 +1,10 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+numbers = []
+for int  in range(10):
+    num = int(input(f"Enter a number{i+1}: "))
+    if num not in numbers:
+        numbers.append(num)
+print("\nNumbers displayed (first occurence only): ")
+for num in numbers:
+    print(num)
